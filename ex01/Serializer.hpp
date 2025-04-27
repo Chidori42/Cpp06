55b21a7efea4d6fe0bcebe1fc24696c5af804473
Chidori42 <elfagrouch9@gmail.com>
2025-04-27 11:53:36 +0100

    lets finish

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -10,8 +10,6 @@ class Serializer{
         Serializer();
     public:
         ~Serializer();
-        Serializer(const Serializer &other);
-        Serializer &operator=(const Serializer &other);
         static uintptr_t serialize(Data* ptr);
         static Data* deserialize(uintptr_t raw);
 };
