3374e2cfd23d77fb835f25151f7e07180cb6febe
Abdellatif El Fagrouch <ael-fagr@e2r5p2.1337.ma>
2025-04-28 10:40:44 +0100

    Finish

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-fagr <ael-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 19:46:06 by ael-fagr          #+#    #+#             */
-/*   Updated: 2025/04/24 23:28:14 by ael-fagr         ###   ########.fr       */
+/*   Updated: 2025/04/28 10:36:08 by ael-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@ int main()
 {
     Base b;
     Base *new_b = b.generate();
-
-    b.identify(new_b);
-    delete new_b;
+    try{
+        b.identify(new_b);
+        delete new_b;
+    }catch(std::exception &e){
+        std::cout << e.what() << std::endl;
+        delete new_b;
+    }
     return (0);
 }
