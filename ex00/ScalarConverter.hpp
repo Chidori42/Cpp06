a67c99cc26af9b7be8970b5c0c60dbdb742f650c
abdellatif el fagrouch <137655886+Chidori42@users.noreply.github.com>
2025-05-18 21:43:23 +0100

    cpp06

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-fagr <ael-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 18:10:11 by ael-fagr          #+#    #+#             */
-/*   Updated: 2025/05/18 21:37:29 by ael-fagr         ###   ########.fr       */
+/*   Updated: 2025/05/18 21:43:18 by ael-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 #define SCALARCONVERTER_HPP
 
 #include <cstdlib>
-#include <string>
-#include <cctype>
+#include <cstring>
 #include <iomanip>
+#include <climits>
 #include <cmath>
 #include <sstream>
 #include <iostream>
